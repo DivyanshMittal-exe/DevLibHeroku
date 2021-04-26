09dbb3123db5a359b6bff9543c0c11920dcc8499
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2021-04-26 12:43:06 +0530

    Debug

--- DevLibrary/settings.py
+++ DevLibrary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dn7ewhgqkh4n!^6t-0*l%el=rehz0^@o6voh#60g0_tnk@$q5x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0','dev-library-iitd.herokuapp.com']
 
